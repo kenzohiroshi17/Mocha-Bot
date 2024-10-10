67b1408574fde3ea134738f0b5ec63fb13111720
Kenzo Hiroshi <glennlotertetabalde12@gmail.com>
2024-10-11 06:15:28 +0800

    Delete scripts/cmds/greet.js

--- scripts/cmds/greet.js
+++
@@ -1,7 +0,0 @@
-const { EasyGoat } =require ("easy-goat") ;
-
-module.exports = EasyGoat({
- name:: "greet",
- author: "Kenzo Hiroshi",
- description: "Greet user", 
- text: "Hello {mention}, How are you today?, Have you a good day."});
